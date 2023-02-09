3c02189a4540c318d0e1246a5422ddbd12b65668
hopypark <hopypark@gmail.com>
2023-02-09 17:41:25 +0900

    Adding Class

--- Week04/src/week04/ch05/StudentTest3.java
+++ Week04/src/week04/ch05/StudentTest3.java
@@ -1,19 +1,15 @@
 package week04.ch05;
 
-public class StudentTest2 {
+public class StudentTest3 {
 	public static void main(String[] args) {
-		
 		Student student1 = new Student();	// 첫 번째 학생 생성
 		Student student2 = new Student();	// 두 번째 학생 생성
 		
-		
-		student1.studentName = "홍길동";
-		System.out.println(student1.getStudentName());
-		
-		
-		
+		student1.studentName = "홍길동";								
 		student2.studentName = "안상수";		// 멤버 변수 사용
-		System.out.println(student2.getStudentName());	// 메서드 사용
+		
+		System.out.println(student1);
+		System.out.println(student2);	// 메서드 사용
 	}
 }
 
