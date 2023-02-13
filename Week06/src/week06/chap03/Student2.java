a4db32a0579e90f6d26dce0421e48fa4e028ef71
hopypark <hopypark@gmail.com>
2023-02-13 14:11:37 +0900

    create new classes

---
+++ Week06/src/week06/chap03/Student2.java
@@ -0,0 +1,33 @@
+package week06.chap03;
+
+public class Student2 {
+	private static int serialNum = 1000;
+	int studentID;			// 학번
+	String studentName;		// 이름
+	int grade;				// 학년
+	String address;			// 사는 곳
+	
+	public Student2() {
+		serialNum++;			// 학생이 생성될 때마다 증가
+		studentID = serialNum;	// 증가된 값을 학번 인스턴스 변수에 대입
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String name) {
+		studentName = name;
+	}	
+	
+	public static int getSerialNum() {		
+		int i = 10;
+		studentName = "이지원";
+		return serialNum;
+	}
+	
+	public static void setSerialNum(int serialNum) {
+		Student2.serialNum = serialNum;
+	}
+}
+
