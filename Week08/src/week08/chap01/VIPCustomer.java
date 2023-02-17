dd5844af24287cdd1192849e163abadb8db3621f
hopypark <hopypark@gmail.com>
2023-02-17 14:43:35 +0900

    Update VIPCustomer.java

--- Week08/src/week08/chap01/VIPCustomer.java
+++ Week08/src/week08/chap01/VIPCustomer.java
@@ -5,13 +5,11 @@ public class VIPCustomer extends Customer {
 	private int agentID;		// VIP 고객 담당 상담원
 	double saleRatio;			// 할인율
 	
-//	public VIPCustomer() {
-//		super();
-//		customerGrade = "VIP";	// 기본 등급
-//		bonusRatio = 0.05;		// 기본 적립 비율 5%
-//		saleRatio = 0.1;		// 할인율 10%
-//		System.out.println("VIPCustomer() 생성자 호출");		
-//	}
+	/*
+	 * public VIPCustomer() { super(); customerGrade = "VIP"; // 기본 등급 bonusRatio =
+	 * 0.05; // 기본 적립 비율 5% saleRatio = 0.1; // 할인율 10%
+	 * System.out.println("VIPCustomer() 생성자 호출"); }
+	 */
 	
 	public VIPCustomer(int customerID, String customerName, int agentID) {
 		super(customerID, customerName);
@@ -24,6 +22,10 @@ public class VIPCustomer extends Customer {
 	
 	public int getAgentID() {
 		return agentID;
+	}
+	
+	public String showVIPInfo() {
+		return super.showCustomerInfo() + " 담당 상담원 아이디는 " + agentID + "입니다.";
 	}
 }
 
