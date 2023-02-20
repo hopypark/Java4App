a5d49ea4f56fe6fefb5543ff3058eaeb91d19a29
hopypark <hopypark@gmail.com>
2023-02-20 14:43:15 +0900

    Add AnimalTest.java for downcasting test

--- Week08/src/week08/chap04/AnimalTest1.java
+++ Week08/src/week08/chap04/AnimalTest1.java
@@ -28,11 +28,12 @@ public class AnimalTest1 {
 	public static void main(String[] args) {
 		AnimalTest1 aTest = new AnimalTest1();
 		
-		Animal animal = new Human();
+//		Animal animal = new Human();
 		
 		aTest.moveAnimal(new Human());
 		aTest.moveAnimal(new Tiger());
 		aTest.moveAnimal(new Eagle());
+
 	}
 	
 	public void moveAnimal(Animal animal) {
