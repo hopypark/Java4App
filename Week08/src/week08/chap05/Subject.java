f6a1d53e47086f190adae6c14f6b2a5f7a1157a2
hopypark <hopypark@gmail.com>
2023-02-20 13:56:50 +0900

    Create Subject.java

---
+++ Week08/src/week08/chap05/Subject.java
@@ -0,0 +1,25 @@
+package week08.chap05;
+
+public class Subject {
+	private int subjectId;
+	private int subjectName;
+	
+	public int getSubjectId() {
+		return subjectId;
+	}
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+	public int getSubjectName() {
+		return subjectName;
+	}
+	public void setSubjectName(int subjectName) {
+		this.subjectName = subjectName;
+	}
+	
+	public void showSubjectInfo() {
+		System.out.println(subjectId + ", " + subjectName);
+	}
+}
+
+
