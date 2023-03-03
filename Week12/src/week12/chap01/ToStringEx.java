5a3e4cc8bd0941b8197884bfa2e87db2118123a9
hopypark <hopypark@gmail.com>
2023-03-03 12:29:24 +0900

    Update ToStringEx.java

--- Week12/src/week12/chap01/ToStringEx.java
+++ Week12/src/week12/chap01/ToStringEx.java
@@ -8,6 +8,11 @@ class Book extends Object {
 		this.bookNumber = bookNumber;
 		this.bookTitle = bookTitle;
 	}
+
+	@Override
+	public String toString() {
+		return bookTitle + ", " + bookNumber;
+	}
 }
 
 public class ToStringEx {
@@ -17,10 +22,6 @@ public class ToStringEx {
 		System.out.println(book1);
 		System.out.println(book1.toString());
 		
-		String str = new String("test");
-		System.out.println(str);
-		Integer i1 = new Integer(100);
-		System.out.println(i1);
 	}
 }
 
