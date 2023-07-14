64f808d9602dbb00aab0218df4e6639a1fec9598
hopypark <hopypark@gmail.com>
2023-07-14 10:38:04 +0900

    Update OperationEx1.java

--- Week03/src/week03/ch01/OperationEx1.java
+++ Week03/src/week03/ch01/OperationEx1.java
@@ -2,7 +2,7 @@ package week03.ch01;
 public class OperationEx1 {
 	public static void main(String[] args) {
 		int mathScore = 90;
-		int engScore = 70;
+		int engScore = 80;
 		
 		int totalScore = mathScore + engScore;
 		System.out.println(totalScore);
