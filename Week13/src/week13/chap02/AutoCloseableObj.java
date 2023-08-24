b52d995897aa734efbbe997d7d81c7f9178cffff
hopypark <hopypark@gmail.com>
2023-08-24 12:37:57 +0900

    Create AutoCloseableObj.java

---
+++ Week13/src/week13/chap02/AutoCloseableObj.java
@@ -0,0 +1,10 @@
+package week13.chap02;
+
+public class AutoCloseableObj implements AutoCloseable {
+	@Override
+	public void close() throws Exception {
+		System.out.println("리소스가 close() 되었습니다.");
+
+	}
+}
+
