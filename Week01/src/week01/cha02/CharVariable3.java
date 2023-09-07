aa03c90d9ef5cc8d54a925e3b6b3be165748df91
hopypark <hopypark@gmail.com>
2023-09-07 15:54:40 +0900

    Create CharVariable3.java

---
+++ Week01/src/week01/cha02/CharVariable3.java
@@ -0,0 +1,16 @@
+package week01.cha02;
+
+public class CharVariable3 {
+	public static void main(String[] args) {
+		int a = 65;
+		int b = -76;
+		//
+		char c1 = 65;
+//		char c2 = -66;
+		
+		System.out.println((char) a);
+		System.out.println((char) b);
+		System.out.println(c1);
+	}
+}
+
