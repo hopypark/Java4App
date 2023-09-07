d805fb9f3a90c632c94bd13a9d1e6c6e358ec4d6
hopypark <hopypark@gmail.com>
2023-09-07 15:54:36 +0900

    Create CharVariable.java

---
+++ Week01/src/week01/cha02/CharVariable.java
@@ -0,0 +1,20 @@
+package week01.cha02;
+
+public class CharVariable {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';	// 문자 자료형 변수에 문자 대입
+		System.out.println(ch1);	// 문자 출력
+		System.out.println((int) ch1); 	// 문자에 해당하는 정수값 출력
+		//
+		char ch2 = 66;	// 문자 자료형 변수에 정수값 대입
+		System.out.println(ch2);// 정수값에 해당하는 문자 출력
+		
+		int ch3 = 67;	// 정수 자료형 변수에 정수값 대입
+		System.out.println(ch3);	// 정수값 출력
+		System.out.println((char) ch3);	// 정수값에 해당하는 문자 출력
+	}
+
+}
+
+
