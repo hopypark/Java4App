09dd526cd09ec00b0d071e97c76cd09c3d59aaaf
hopypark <hopypark@gmail.com>
2023-09-07 15:54:39 +0900

    Create CharVariable2.java

---
+++ Week01/src/week01/cha02/CharVariable2.java
@@ -0,0 +1,13 @@
+package week01.cha02;
+
+public class CharVariable2 {
+	public static void main(String[] args) {
+		char ch1 = '한';
+		char ch2 = '\uD55C';
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
+
+
