b68fccb0580a7dde87e78dfde1e34a875746ce7b
hopypark <hopypark@gmail.com>
2023-09-07 15:54:51 +0900

    Create FloatVariable.java

---
+++ Week01/src/week01/cha02/FloatVariable.java
@@ -0,0 +1,12 @@
+package week01.cha02;
+
+public class FloatVariable {
+	public static void main(String[] args) {
+		float fnum1 = 0.001F;
+		float fnum2 = 0.001F;
+		float fnum3 = 0.0001F;
+		
+		System.out.println(fnum1 + fnum1 + fnum3);
+	}
+}
+
