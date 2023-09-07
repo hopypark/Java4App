fe48736493876309e9dc1531c1a68251399fa2db
hopypark <hopypark@gmail.com>
2023-09-07 15:54:57 +0900

    Create Variable1.java

---
+++ Week01/src/week01/cha02/Variable1.java
@@ -0,0 +1,12 @@
+package week01.cha02;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int age;	// 숫자(나이)를 저장하기 위한 변수 age 선언
+		age = 35;	// age 변수에 35를 대입
+		System.out.println(age); 	// 나이 출력
+	}
+
+}
+
