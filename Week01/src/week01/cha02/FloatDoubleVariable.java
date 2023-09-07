7cc47d3fe509a41a75236059007ffbc4b24eaff3
hopypark <hopypark@gmail.com>
2023-09-07 15:54:46 +0900

    Create FloatDoubleVariable.java

---
+++ Week01/src/week01/cha02/FloatDoubleVariable.java
@@ -0,0 +1,14 @@
+package week01.cha02;
+
+public class FloatDoubleVariable {
+	public static void main(String[] args) {
+		float fnum = 1.23456789f;
+		double dnum = 1.234567890123456789;
+		
+		System.out.println("Float : " + fnum);
+		System.out.println("Double: " + dnum);
+	}
+}
+
+
+
